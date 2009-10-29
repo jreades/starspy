c6ee7516e8d4b7fa926049171c63bc04a0f1c9f7
pyramidliuyin <pyramidliuyin@a0262ff6-7a6d-11de-9755-51fe06bdd687>
2009-10-29 07:35:24 +0000

    add the test

--- branches/gph471f09/liu/test.py
+++ branches/gph471f09/liu/test.py
@@ -1,2 +1,17 @@
-p = 12
-print p
+from Tkinter import *
+
+# set up the window itself
+top = Tk()
+top.title("Frame holding a label and 2 buttons")
+
+f=Frame(top)
+Button(f, text="Rank-Size").pack(side=TOP)
+Button(f,text="Rank-Clock",command=canvas.pack()).pack(side=TOP)
+Button(f, text="Quit", command=f.quit).pack(side=TOP)
+
+#set up the canvas
+canvas = Canvas(f, width=500, height=500, bg="white")
+canvas.create_oval(50,50,450,450,fill="black")
+
+f.pack()
+top.mainloop()
