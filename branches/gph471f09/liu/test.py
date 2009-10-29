509c4c28a70d2e1ec551cfce9acd6dec2e2ab53b
pyramidliuyin <pyramidliuyin@a0262ff6-7a6d-11de-9755-51fe06bdd687>
2009-10-29 21:47:10 +0000

    the log message

--- branches/gph471f09/liu/test.py
+++ branches/gph471f09/liu/test.py
@@ -1,17 +1,25 @@
 from Tkinter import *
+from math import *
 
 # set up the window itself
 top = Tk()
 top.title("Frame holding a label and 2 buttons")
 
 f=Frame(top)
-Button(f, text="Rank-Size").pack(side=TOP)
-Button(f,text="Rank-Clock",command=canvas.pack()).pack(side=TOP)
-Button(f, text="Quit", command=f.quit).pack(side=TOP)
+Button(f, text="Rank-Size").pack(side=LEFT)
+Button(f,text="Rank-Clock").pack(side=LEFT)
+Button(f, text="Quit", command=f.quit).pack(side=LEFT)
 
 #set up the canvas
-canvas = Canvas(f, width=500, height=500, bg="white")
-canvas.create_oval(50,50,450,450,fill="black")
+canvas = Canvas(f, width=800, height=800, bg="white")
+canvas.create_oval(50,50,750,750,fill="black")
 
+def drawlines(n):
+	ang = 2*pi/n
+	for num in range(n):
+		canvas.create_line(400,400,400+350*cos(ang*num-pi/2),400+350*sin(ang*num-pi/2),fill='white')
+drawlines(10)
+
+canvas.pack()
 f.pack()
 top.mainloop()
