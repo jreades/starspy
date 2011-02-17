2cb334af22386657fb4e1b2daf4a47c5d5f310e3
schmidtc <schmidtc@a0262ff6-7a6d-11de-9755-51fe06bdd687>
2011-02-17 21:56:40 +0000

    inital move of wxVisualization code from geodacenter SVN to stars SVN.

---
+++ stars/visualization/wxStars/mapModels.py
@@ -0,0 +1,69 @@
+import pysal
+from model import AbstractModel
+from layers import BaseLayer, PointLayer, PolygonLayer
+from transforms import WorldToViewTransform
+
+class MapModel(WorldToViewTransform):
+    """ Base Model for all Maps 
+        Maintains the state of the map,
+            The current view extent, layer order, etc
+        Handles Map Events,
+            Pan, Zoom, Selection, etc.
+    """
+    def __init__(self,layers=[],initial_size=(500,500)):
+        self._data = {'selectionLayer':0,'layers':[],'world_extent':pysal.cg.Rectangle(0,0,0,0)}
+        WorldToViewTransform.__init__(self,self.world_extent,*initial_size)
+        for layer in layers:
+            self.addLayer(layer)
+        self.zoom_to_world()
+    def __len__(self):
+        """
+        Returns len(self.layers)
+        """
+        return len(self._data['layers'])
+    def _layerListener(self,layer,tag):
+        """
+        Propgates layer events to anyone listening to the map.
+        """
+        lid = self._data['layers'].index(layer)
+        self.update('%s:%d'%(tag,lid))
+    def addLayer(self,layer):
+        """
+        Add a layer to the current map.
+        Layers are made selectable when added.
+        """
+        if issubclass(type(layer),BaseLayer) and layer not in self._data['layers']:
+            layer.is_selectable = True
+            self._data['layers'].append(layer)
+            layer.addListener(self._layerListener)
+            self._data['world_extent'] = pysal.cg.Rectangle(0,0,0,0)
+            self.update('layers')
+    @property
+    def layers(self):
+        return self._data['layers'][:]
+    @property
+    def world_extent(self):
+        if not self._data['world_extent']:
+            n = len(self)
+            if n == 1:
+                self._data['world_extent'] = self._data['layers'][0].extent
+            elif n > 1:
+                self._data['world_extent'] = pysal.cg.get_bounding_box([layer.extent for layer in self.layers])
+        return self._data['world_extent']
+    def zoom_to_world(self):
+        """
+        Zoom the current transform to the extent of the world.
+        """
+        self.extent = self.world_extent
+
+
+
+if __name__=='__main__':
+    import pysal
+    stl = pysal.open('/Users/charlie/Documents/data/stl_hom/stl_hom.shp').read()
+    polys = PolygonLayer(stl)
+    #pts = PointLayer([p.centroid for p in stl])
+
+    mapObj = MapModel([polys])
+    #mapObj.addLayer(polys)
+    #mapObj.addLayer(pts)
---
+++ stars/visualization/wxStars/wxLayers.py
@@ -0,0 +1,139 @@
+"""
+wxLayers
+
+wxLayers implement drawing of stars layers in wx
+"""
+import wx
+import layers
+
+class wxPointLayer:
+    def __init__(self,layer):
+        if not isinstance(layer,layers.PointLayer):
+            raise TypeError, "Layer must be instance of PointLayer"
+        self.layer = layer
+        self.trns = 0
+        self.radius = 5
+    def draw_set(self,gc,matrix,ids):
+        r = self.radius#*transform.scale #see option 2 below
+        data = self.layer.data
+        for i in ids:
+            pt = data[i]
+            ### Two options for drawing points.
+            ### First is to Draw with an Ellipse
+            x,y = matrix.TransformPoint(*pt)
+            gc.DrawEllipse(x,y,r,r)
+            ### Second is to Draw with a Graphics Path,
+            ### It seems that you need to create a new Path Each time
+            ### Otherwise the lines are drawn funny.
+            ### Make sure you scale the radius above is you use the 2nd option.
+            #x,y = pt
+            #pth = gc.CreatePath()
+            #pth.AddCircle(x,y,r)
+            #pth.CloseSubpath()
+            #pth.Transform(matrix)
+            #gc.FillPath(pth)
+            #gc.StrokePath(pth)
+        return gc
+    def draw(self,transform):
+        w,h = transform.pixel_size
+        buff = wx.EmptyBitmapRGBA(w,h,alpha=self.trns)
+        dc = wx.MemoryDC()
+        dc.SelectObject(buff)
+        gc = wx.GraphicsContext.Create(dc)
+        gc.SetPen( gc.CreatePen(wx.Pen(wx.Colour(0,0,0,255),1)) )
+        matrix = gc.CreateMatrix()
+        matrix.Scale(1./transform.scale,1./-transform.scale) #first transform is applied last
+        matrix.Translate(*transform.offset)                   #last transform is applied first
+        if self.layer.classification and self.layer.colors:
+            cl = self.layer.classification
+            cs = self.layer.colors
+        else:
+            class EmptyCL:
+                classes = [range(len(self.layer.data))]
+            cl = EmptyCL()
+            cs = {0:(255,0,0)}
+        for i,cls in enumerate(cl.classes):
+            r,g,b = cs[i]
+            gc.SetBrush( gc.CreateBrush(wx.Brush(wx.Colour(r,g,b,255))) )
+            gc = self.draw_set(gc,matrix,cls)
+        return buff
+    def draw_selection(self,transform):
+        w,h = transform.pixel_size
+        r = self.radius#*transform.scale #see option 2 below
+        buff = wx.EmptyBitmapRGBA(w,h,alpha=self.trns)
+        dc = wx.MemoryDC()
+        dc.SelectObject(buff)
+        gc = wx.GraphicsContext.Create(dc)
+        gc.SetPen( gc.CreatePen(wx.Pen(wx.Colour(0,0,0,255),1)) )
+        gc.SetBrush( gc.CreateBrush(wx.Brush(wx.Colour(255,255,0,255))) )
+        matrix = gc.CreateMatrix()
+        matrix.Scale(1./transform.scale,1./-transform.scale) #first transform is applied last
+        matrix.Translate(*transform.offset)                   #last transform is applied first
+        gc = self.draw_set(gc,matrix,self.layer.selection)
+        return buff
+class wxPolygonLayer:
+    def __init__(self,layer):
+        if not isinstance(layer,layers.PolygonLayer):
+            raise TypeError, "Layer must be instance of PolygonLayer"
+        self.layer = layer
+        self.trns = 0
+    def draw_set(self,pth,ids):
+        data = self.layer.data
+        for i in ids:
+            poly = data[i]
+            parts = poly.parts
+            if poly.holes[0]:
+                parts = parts + poly.holes
+            for part in parts:
+                x,y = part[0]
+                pth.MoveToPoint(x,y)
+                for x,y in part[1:]:
+                    pth.AddLineToPoint(x,y)
+                pth.CloseSubpath()
+        return pth
+    def draw(self,transform):
+        w,h = transform.pixel_size
+        buff = wx.EmptyBitmapRGBA(w,h,alpha=self.trns)
+        dc = wx.MemoryDC()
+        dc.SelectObject(buff)
+        gc = wx.GraphicsContext.Create(dc)
+        gc.SetPen( gc.CreatePen(wx.Pen(wx.Colour(0,0,0,255),1)) )
+        matrix = gc.CreateMatrix()
+        matrix.Scale(1./transform.scale,1./-transform.scale) #first transform is applied last
+        matrix.Translate(*transform.offset)                   #last transform is applied first
+        if self.layer.classification and self.layer.colors:
+            cl = self.layer.classification
+            cs = self.layer.colors
+        else:
+            class EmptyCL:
+                classes = [range(len(self.layer.data))]
+            cl = EmptyCL()
+            cs = {0:(255,0,0)}
+        for i,cls in enumerate(cl.classes):
+            r,g,b = cs[i]
+            pth = gc.CreatePath()
+            pth = self.draw_set(pth,cls)
+            pth.Transform(matrix)
+            gc.SetBrush( gc.CreateBrush(wx.Brush(wx.Colour(r,g,b,255))) )
+            gc.FillPath(pth)
+            gc.StrokePath(pth)
+        return buff
+    def draw_selection(self,transform):
+        w,h = transform.pixel_size
+        buff = wx.EmptyBitmapRGBA(w,h,alpha=self.trns)
+        dc = wx.MemoryDC()
+        dc.SelectObject(buff)
+        gc = wx.GraphicsContext.Create(dc)
+        gc.SetPen( gc.CreatePen(wx.Pen(wx.Colour(0,0,0,255),1)) )
+        gc.SetBrush( gc.CreateBrush(wx.Brush(wx.Colour(255,255,0,255))) )
+        matrix = gc.CreateMatrix()
+        matrix.Scale(1./transform.scale,1./-transform.scale) #first transform is applied last
+        matrix.Translate(*transform.offset)                   #last transform is applied first
+        pth = gc.CreatePath()
+        pth = self.draw_set(pth,self.layer.selection)
+        pth.Transform(matrix)
+        gc.FillPath(pth)
+        gc.StrokePath(pth)
+        return buff
+
+wxLayers = {'PolygonLayer':wxPolygonLayer,'PointLayer':wxPointLayer}
