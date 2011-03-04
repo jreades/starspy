93277dc5127afa84280e81a5f19f4e5cd658f35a
schmidtc <schmidtc@a0262ff6-7a6d-11de-9755-51fe06bdd687>
2011-03-04 21:35:40 +0000

    added serge's spatial indexing. cleaned up table view code

--- stars/gui/tableViewer/control.py
+++ stars/gui/tableViewer/control.py
@@ -15,45 +15,45 @@ def spec2str(spec):
     elif typ == 'l': value = str(value)[0].upper()
     else: value = str(value)[:s].ljust(s, 'x')
     return value
-class DbfTableList(wx.ListCtrl,stars.visualization.model.AbstractModel):
-    def __init__(self,parent,dbf):
+class DbfTableList(wx.ListCtrl):
+    def __init__(self,parent,layer):
         wx.ListCtrl.__init__(self,parent,style=wx.LC_REPORT|wx.LC_VIRTUAL)
-        stars.visualization.model.AbstractModel.__init__(self)
-        self._modelData['selection'] = set()
 
         #bindings
         self.Bind(wx.EVT_IDLE,self.onIdle)
         self.Bind(wx.EVT_CHAR,self.onKey)
 
-        self.db = dbf
+        self.layer = layer
+        self.db = layer.data_table
         #Background Styles
         self.grey = wx.ListItemAttr(colBack=wx.Colour(237,243,254))
         self.white = wx.ListItemAttr(colBack=wx.Colour(255,255,255))
         # Fill the Header names with the max width for each field based on the spec
         # Use -2 for the width tells wx to adjust the column widths based on the header.
         # We'll capture these width (based on current font) Then we'll clear it all out.
-        for field,spec in zip(dbf.header,dbf.field_spec):
+        for field,spec in zip(self.db.header,self.db.field_spec):
             self.InsertColumn(-1,spec2str(spec),width=-2)
-        widths = [self.GetColumnWidth(i) for i in range(len(dbf.header))]
+        widths = [self.GetColumnWidth(i) for i in range(len(self.db.header))]
         self.ClearAll()
-        for field,w in zip(dbf.header,widths):
+        for field,w in zip(self.db.header,widths):
             self.InsertColumn(-1,field,width=w)
-        self.SetItemCount(len(dbf))
-    def __set_selection(self,value):
-        sel = self.selection
-        sel.clear()
-        nxt = -1
-        while 1:
-            nxt = self.GetNextSelected(nxt)
-            if nxt == -1:
-                break
-            else:
-                self.Select(nxt,False)
-        for x in value:
-            self.Select(x)
-    def __get_selection(self):
-        return self._modelData['selection']
-    selection = property(fget=__get_selection,fset=__set_selection)
+        self.SetItemCount(len(self.db))
+
+        layer.addListener(self.updateLayer)
+    def updateLayer(self,mdl,tag):
+        if tag == 'selection':
+            value = self.layer.selection
+            nxt = -1
+            # Unselect everything first.
+            while 1:
+                nxt = self.GetNextSelected(nxt)
+                if nxt == -1:
+                    break
+                else:
+                    self.Select(nxt,False)
+            # Now select the new rows.
+            for x in value:
+                self.Select(x)
 
     def OnGetItemText(self,item,column):
         """
@@ -72,7 +72,11 @@ class DbfTableList(wx.ListCtrl,stars.visualization.model.AbstractModel):
         else:
             return self.white
     def onIdle(self,evt):
-        sel = self.selection
+        """
+        Because we are using a virtual list, selection events will not work as expected.
+        Instead while Idle we'll check to see if the selection changed, if so, update the layer.
+        """
+        sel = self.layer.selection
         newSel = set()
         nxt = -1
         while 1:
@@ -82,18 +86,15 @@ class DbfTableList(wx.ListCtrl,stars.visualization.model.AbstractModel):
             else:
                 newSel.add(nxt)
         if sel != newSel:
-            #print "Remove:",sel.difference(newSel)
-            #print "Add:",newSel.difference(sel)
-            self._modelData['selection'] = newSel
-            self.update('selection')
+            self.layer.selection = newSel
     def onKey(self,evt):
-        self.selection = set(random.sample(xrange(len(self.db)),20))
+        self.layer.selection = set(random.sample(xrange(len(self.db)),20))
         
     
 class TableViewer(wx.Frame):
-    def __init__(self,parent,dbf):
+    def __init__(self,parent,layer):
         wx.Frame.__init__(self,parent)
-        self.model = DbfTableList(self,dbf)
+        self.model = DbfTableList(self,layer)
         self.Bind(wx.EVT_CLOSE,self.toggleShown)
     def toggleShown(self,evt):
         self.Show(self.IsShown()^True)
