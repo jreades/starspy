62cdcec6536b9144eb0b705a2eef2e23ca45dffd
jamestaysom <jamestaysom@a0262ff6-7a6d-11de-9755-51fe06bdd687>
2011-11-17 16:27:10 +0000

    "This is the code that should read in a shapfile"

---
+++ spacetime/readshapefile.py
@@ -0,0 +1,12 @@
+import tempfile
+f = tempfile.NamedTemporaryFile(suffix = '.shp'); fname = f.name; f.close()
+import pysal
+i = pysal.open('shapefile.shp', 'r')
+o = pysal.open(fname, 'w')
+for shp in i:
+    o.write(shp)
+o.close
+open('shapefile.shp','rb').read() == open(fname,'rb').read()
+open('shapefile.shx','rb').read() == open(fname[:-1]+'x','rb').read()
+import os
+os.remove(fname); os.remove(fname.replace('.shp','.shx'))
