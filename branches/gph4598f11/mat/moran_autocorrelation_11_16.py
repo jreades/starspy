50b40e76f8ae8634a81f2c021020816458dddc8b
sanjay.buet@gmail.com <sanjay.buet@gmail.com@a0262ff6-7a6d-11de-9755-51fe06bdd687>
2011-11-20 17:44:31 +0000

    last update

---
+++ branches/gph4598f11/mat/moran_autocorrelation_11_16.py
@@ -0,0 +1,130 @@
+
+import sys,math,random,time
+from copy import deepcopy 
+import numpy as np
+#from pylab import *
+
+
+import matplotlib
+matplotlib.use('WXAgg')
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.backends.backend_wx import NavigationToolbar2Wx
+from matplotlib.figure import Figure
+import wx
+
+
+class Moran_scatter: #(wx.Frame):
+
+#    def __init__(self, args, **kwargs):
+#        super(Moran_scatter, self).__init__(args, **kwargs) 
+
+    def __init__(self, figure, comma, gal):     
+
+        '''
+        Constructor
+        '''
+
+        read_data = open("C:/Users/Rumpa/stl_hom.txt", "r")
+        read_data = open(comma, "r")
+
+        read_data.readline()
+        read_data.readline()
+        
+        ch = []
+        for line in read_data:
+            temp = line.split(",")
+            ch.append(float(temp[3]))
+
+        y = np.array(ch)
+        ymean = y.mean()
+        ystd = y.std()
+        z = (y - ymean)/ystd
+        
+        read_data.close()
+        
+        read_gal = open("C:/Users/Rumpa/stl.gal", "r")
+        read_gal = open(gal, "r")
+        numNode = int(read_gal.readline())
+        
+        wz = []
+        zi = list(z)
+        for i in range(numNode):
+            loc, num = read_gal.readline().split(" ")
+            loc = int(loc)
+            num = int(num)
+            
+            wzi = 0
+            temp = read_gal.readline().split(" ")
+            for k in temp:
+                index = int(k)-1
+                wzi += zi[index]/num
+            wz.append(wzi)
+        
+        wz = np.array(wz)
+        print z
+        print wz
+        
+        read_gal.close()
+
+
+        self.figure = figure
+        self.axes = self.figure.add_subplot(111)
+	self.axes.set_xlabel('Zi')
+	self.axes.set_ylabel('Wij*Zj')
+
+        self.axes.scatter(z,wz)
+        
+        slope, yint = self.LineFit(z,wz)
+        regression = slope*z + yint
+        self.axes.plot(z,regression)
+
+        
+#        self.canvas = FigureCanvas(self, -1, self.figure)
+
+        
+#        figure(1)
+#        scatter(z,wz,c='r')
+#        
+#        slope, yint = self.LineFit(z,wz)
+#        regression = slope*z + yint
+#        figure(1)
+#        self.axes.plot(z,regression)
+#        show()
+
+
+#        self.sizer = wx.BoxSizer(wx.VERTICAL)
+#        self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
+#        self.SetSizer(self.sizer)
+#        self.Fit()
+#
+#
+##        panel = wx.Panel(self)
+##        
+##        panel.SetSizer(self.sizer)
+#        self.SetSize((600, 400))
+#        self.SetTitle('PySal Plots')
+#        self.Centre()
+#        self.Show(True)
+        
+
+    def LineFit(self,x,y):
+        ''' Returns slope and y-intercept of linear fit to (x,y) data set'''
+        xavg = x.mean()
+        slope = (y*(x-xavg)).sum()/(x*(x-xavg)).sum()
+        yint = y.mean()-slope*xavg
+        return slope, yint
+
+
+
+def main():
+    ex = wx.App()
+    Moran_scatter(None)
+    ex.MainLoop()
+
+
+
+if __name__=="__main__":
+    main()
+    
+    
+
---
+++ branches/gph4598f11/mat/moran_permutation_11_16.py
@@ -0,0 +1,54 @@
+
+
+import pysal
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+import matplotlib
+matplotlib.use('WXAgg')
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.backends.backend_wx import NavigationToolbar2Wx
+from matplotlib.figure import Figure
+
+class Moran_histogram:
+  
+      def __init__(self, figure, mi):
+	 self.figure = figure
+	 self.axes = self.figure.add_subplot(111)
+	 #mi = pysal.Moran(y,  w, permutations=9999)
+
+
+
+
+         # the histogram of the data
+         n, bins, patches = self.axes.hist(mi.sim, 50, normed=1, facecolor='green', alpha=0.9)
+
+         # add a 'best fit' line
+         y = mlab.normpdf(bins, mi.EI_sim, mi.sim.std())
+         l = self.axes.plot(bins, y, 'r--', linewidth=1)
+         l2 = self.axes.plot(mi.I*np.ones(20), max(n)*np.array(range(20))/19, 'b', linewidth =2)
+
+         #self.axes.set_xlabel('Permutation')
+         #self.axes.set_ylabel('Density')
+         # plt.title(r"Permutation of Moran's I")
+         #self.axes.grid(True)
+
+         #plt.show()
+
+
+def main():
+    ex = wx.App()
+    Moran_histogram(None)
+    ex.MainLoop()
+
+
+
+
+if __name__=="__main__":
+    main()
+
+
+
+
+
