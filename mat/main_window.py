2aff2344ad73010da6f880c9f9c3d81edcd63a5a
Daehyun.You@gmail.com <Daehyun.You@gmail.com@a0262ff6-7a6d-11de-9755-51fe06bdd687>
2011-11-29 16:41:02 +0000

    Insert map functions for showing map and upgrade some functions.

--- mat/main_window.py
+++ mat/main_window.py
@@ -20,6 +20,8 @@ import wx
 from moran_autocorrelation import *
 from moran_pvalue import *
 from moran_boxplot import *
+from moran_permutation_11_16 import *
+from draw_shape import *
 
 class Project_GPH(wx.Frame):
   
@@ -49,7 +51,7 @@ class Project_GPH(wx.Frame):
 #        plotMenu.add_command(label="Box-Plot", command=self.onExit)
 #        menubar.add_cascade(label="Plot", menu=plotMenu)
 
-        self.commapath = ""
+        self.w = None
         self.galpath = ""
         self.mi = None
         self.localmi = None
@@ -57,12 +59,15 @@ class Project_GPH(wx.Frame):
         menubar = wx.MenuBar()
         fileMenu = wx.Menu()
         fitem1 = fileMenu.Append(wx.ID_ANY, 'Open GAL', 'Open GAL')
-        fitem2 = fileMenu.Append(wx.ID_ANY, 'Open TXT', 'Open Attribute')
-        fitem3 = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
+        fitem2 = fileMenu.Append(wx.ID_ANY, 'Open Shapefile', 'Open Shapefile (.shp)')
+        fitem3 = fileMenu.Append(wx.ID_ANY, 'Open TXT', 'Open Attribute')
+        fitem4 = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
 
         self.Bind(wx.EVT_MENU, self.openGAL, fitem1)
-        self.Bind(wx.EVT_MENU, self.openTXT, fitem2)
-        self.Bind(wx.EVT_MENU, self.onExit, fitem3)
+        self.Bind(wx.EVT_MENU, self.openSHP, fitem2)
+        self.Bind(wx.EVT_MENU, self.openTXT, fitem3)
+        self.Bind(wx.EVT_MENU, self.onExit, fitem4)
+
         
         menubar.Append(fileMenu, '&File')
         
@@ -70,35 +75,42 @@ class Project_GPH(wx.Frame):
         plot1 = plotMenu.Append(wx.ID_ANY, "Moran's I Scatter", "Show Moran's I Scatter Plot")
         plot2 = plotMenu.Append(wx.ID_ANY, "Moran's I p-values", "Show Moran's I p-values")
         plot3 = plotMenu.Append(wx.ID_EXIT, "Box-Plot", "Show Box-Plot")
+        plot4 = plotMenu.Append(wx.ID_ANY, "Permutation of Moran's I", "Show Histogram of Moran's I")
 
         self.Bind(wx.EVT_MENU, self.showScatter, plot1)
         self.Bind(wx.EVT_MENU, self.showPvalue, plot2)
         self.Bind(wx.EVT_MENU, self.showBoxplot, plot3)
+        self.Bind(wx.EVT_MENU, self.showHistogram, plot4)
         
         menubar.Append(plotMenu, '&Plots')
         self.SetMenuBar(menubar)
 
 
-        main = wx.BoxSizer(wx.HORIZONTAL)
+        #main = wx.BoxSizer(wx.HORIZONTAL)
+        main = wx.GridBagSizer(1, 3)
         
         panel = wx.Panel(self, -1)
         
         
         stat = wx.BoxSizer(wx.VERTICAL)
+        
+        self.ch1 = wx.ComboBox(panel, size=(150, -1), style=wx.CB_READONLY)
+        self.Bind(wx.EVT_COMBOBOX, self.calPysal, self.ch1)
         st1 = wx.StaticText(panel, label="Moran's I: ")
         st2 = wx.StaticText(panel, label='Y mean: ')
         st3 = wx.StaticText(panel, label='Y Standard Deviation: ')
         st4 = wx.StaticText(panel, label='Number of points: ')
-        stat.Add(st1, flag=wx.RIGHT, border=8)
-        stat.Add(st2, flag=wx.RIGHT, border=8)
-        stat.Add(st3, flag=wx.RIGHT, border=8)
-        stat.Add(st4, flag=wx.RIGHT, border=8)
+        stat.Add(self.ch1, flag=wx.LEFT, border=8)
+        stat.Add(st1, flag=wx.LEFT, border=8)
+        stat.Add(st2, flag=wx.LEFT, border=8)
+        stat.Add(st3, flag=wx.LEFT, border=8)
+        stat.Add(st4, flag=wx.LEFT, border=8)
         
-        main.Add(stat, flag=wx.LEFT|wx.EXPAND,border=10)
+        
         
         
         self.infPanel = wx.Panel(panel, -1)
-        figure = Figure(figsize=(6, 4), dpi=80)
+        figure = Figure(figsize=(4, 4), dpi=100)
 #        moran = Moran_scatter(figure)
 #        self.figure = moran.figure
         self.figure = figure
@@ -106,12 +118,23 @@ class Project_GPH(wx.Frame):
         
         plot = wx.BoxSizer(wx.VERTICAL)
         plot.Add(self.canvas, flag=wx.Left, border=10) 
-        
+             
         self.infPanel.SetSizer(plot)
-        main.Add(self.infPanel, 0.6, wx.EXPAND | wx.RIGHT, 20)
+        
+        
+
+        self.shpPanel = wx.Panel(panel,size=(400,400))
+        
+        
+        main.Add(self.shpPanel, pos=(0,0), flag=wx.RIGHT) # | wx.RIGHT, 600)
+        main.Add(self.infPanel, pos=(0,1), flag=wx.EXPAND|wx.RIGHT,border=10)
+        main.Add(stat, pos=(0,2), flag=wx.LEFT|wx.EXPAND,border=10)
+        
+        
+        
         panel.SetSizer(main)
 
-        self.SetSize((600, 400))
+        self.SetSize((1100, 480))
         self.SetTitle('PySal Plots')
         self.Centre()
         self.Show(True)
@@ -119,60 +142,98 @@ class Project_GPH(wx.Frame):
         
     def showScatter(self,e):
         if self.mi <> None:            
-            figure = Figure(figsize=(6, 4), dpi=80)
-            moran = Moran_scatter(figure, self.commapath, self.galpath)
+            figure = Figure(figsize=(4, 4), dpi=100)
+            moran = Moran_scatter(figure, self.galpath, self.y)
             self.figure = moran.figure
             self.canvas = FigureCanvas(self.infPanel, -1, self.figure)        
  
     def showPvalue(self,e):
         if self.localmi <> None:
-            figure = Figure(figsize=(6, 4), dpi=80)
+            figure = Figure(figsize=(4, 4), dpi=100)
             moran = Moran_pvalue(figure, self.localmi)
             self.figure = moran.figure
             self.canvas = FigureCanvas(self.infPanel, -1, self.figure)   
             
     def showBoxplot(self,e):
         if self.localmi <> None:
-            figure = Figure(figsize=(6, 4), dpi=80)
+            figure = Figure(figsize=(4, 4), dpi=100)
             moran = Moran_boxplot(figure, self.y)
             self.figure = moran.figure
             self.canvas = FigureCanvas(self.infPanel, -1, self.figure)       
-        
-    def openPysal(self):
 
-        if len(self.commapath) > 0 and len(self.galpath) > 0:
-            f = pysal.open(self.commapath)
-            self.y = np.array(f.by_col['HR8893'])
-            print "y-values"
-            print self.y
-            w = pysal.open(self.galpath).read()
+    def showHistogram(self,e):
+        if self.mi<> None:
+            figure = Figure(figsize=(4, 4), dpi=100)
+            moran = Moran_histogram(figure,self.mi)
+            self.figure = moran.figure
+            self.canvas = FigureCanvas(self.infPanel, -1, self.figure)
+
+    def openSHP(self, e):
+        wildcard = "Shape File(.shp)|*.shp"
+        dlg = wx.FileDialog(self,"Choose a shape file",os.getcwd(),"",wildcard,wx.OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.shapepath = dlg.GetPath()
+            print "%s" %(self.shapepath)
             
-            self.mi = pysal.Moran(self.y,w)
-            self.localmi = pysal.Moran_Local(self.y,w)
-            print self.mi.I
-            print self.mi.EI
-            print self.mi.p_norm
+            if issubclass(type(self.shapepath),basestring):              
+                geo = pysal.open(self.shapepath,'r')          
+            self.geo = geo          
+    #        if issubclass(type(w),basestring):             
+    #            w = pysal.open(w,'r').read()          
+    #        self.w = w       
+            self.shpPanel = WeightsMap(self,self.geo,self.w) 
+            
+            fname = self.shapepath.replace(".shp",".dbf")
+            self.db = pysal.open(fname,'r')
+            self.ch1.AppendItems(self.db.header)
+            
+        
+        dlg.Destroy()
+
 
     def openGAL(self, e):
         dlg = wx.FileDialog(self,"Choose .gal file",os.getcwd(),"","*.gal",wx.OPEN)
         if dlg.ShowModal() == wx.ID_OK:
             self.galpath = dlg.GetPath()
             print "%s" %(self.galpath)
+            self.w = pysal.open(self.galpath).read()
+            print "self.w"
+            print self.w
             
         dlg.Destroy()
-        self.openPysal()
+        #self.openPysal()
         #self.fileGAL = tkFileDialog.askopenfilename(filetypes = [('.gal files','.gal')], title = 'Open GAL',initialdir='./' )
 
     def openTXT(self, e):
         wildcard = "Comma Delimited File(.txt)|*.txt|Comma Delimited File(.csv)|*.csv"
         dlg = wx.FileDialog(self,"Choose a comma delimited file",os.getcwd(),"",wildcard,wx.OPEN)
         if dlg.ShowModal() == wx.ID_OK:
-            self.commapath = dlg.GetPath()
-            print "%s" %(self.commapath)
+            commapath = dlg.GetPath()
+            print "%s" %(commapath)
+            
+            self.db = pysal.open(commapath)
+            self.ch1.AppendItems(self.db.header)
+            
             
         dlg.Destroy()
-        self.openPysal()
+        #self.openPysal()
         #self.fileTXT = tkFileDialog.askopenfilename(filetypes = [('.csv files','.txt'),('.csv files','.csv')], title = 'Open Comma Delimited',initialdir='./' )
+
+    def calPysal(self, e):
+        i = self.ch1.GetSelection()
+        if i > -1 and self.w <> None:
+            cname = str(self.ch1.GetString(i))
+            self.y = np.array(self.db.by_col[cname])
+                
+            print "y-values"
+            print self.y
+#            self.w = pysal.open(self.galpath).read()
+            
+            self.mi = pysal.Moran(self.y, self.w)
+            self.localmi = pysal.Moran_Local(self.y, self.w)
+            print self.mi.I
+            print self.mi.EI
+            print self.mi.p_norm
 
     def onExit(self, e):
         self.Close()
