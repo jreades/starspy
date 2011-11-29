aed27956d8fe4f99dfbe2c21fb03c72b4acdf6e7
james.taysom@gmail.com <james.taysom@gmail.com@a0262ff6-7a6d-11de-9755-51fe06bdd687>
2011-11-29 16:16:00 +0000

    Changed for loop to produce list "centroid"  that has [point number, (x,y)]

--- branches/gph4598f11/spacetime/readshapefile.py
+++ branches/gph4598f11/spacetime/readshapefile.py
@@ -3,8 +3,14 @@ f = tempfile.NamedTemporaryFile(suffix = '.shp'); fname = f.name; f.close()
 import pysal
 i = pysal.open('shapefile.shp', 'r')
 o = pysal.open(fname, 'w')
+c = []
+p =0
+centroid = []
 for shp in i:
     o.write(shp)
+    c = chp.centroid
+    c = shp.append([p,c])
+    p = p+1
 o.close()
 open('shapefile.shp','rb').read() == open(fname,'rb').read()
 open('shapefile.shx','rb').read() == open(fname[:-1]+'x','rb').read()
