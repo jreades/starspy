0b557d1ad0bc31bc372674563aab90084671f8d0
james.taysom@gmail.com <james.taysom@gmail.com@a0262ff6-7a6d-11de-9755-51fe06bdd687>
2011-11-30 18:58:52 +0000

    Complete overhaul of shapefile reading process. Outputs a table that has the [(point number), (centroid x, centroid y), [info from database file]]

--- branches/gph4598f11/spacetime/readshapefile.py
+++ branches/gph4598f11/spacetime/readshapefile.py
@@ -1,18 +1,10 @@
-import tempfile
-f = tempfile.NamedTemporaryFile(suffix = '.shp'); fname = f.name; f.close()
-import pysal
-i = pysal.open('shapefile.shp', 'r')
-o = pysal.open(fname, 'w')
-c = []
+shp = pysal.open('Arizona_Counties_2010.shp', 'r')
+db = pysal.open('Arizona_Counties_2010.dbf')
+table = []
 p =0
-centroid = []
-for shp in i:
-    o.write(shp)
-    c = chp.centroid
-    c = shp.append([p,c])
-    p = p+1
-o.close()
-open('shapefile.shp','rb').read() == open(fname,'rb').read()
-open('shapefile.shx','rb').read() == open(fname[:-1]+'x','rb').read()
-import os
-os.remove(fname); os.remove(fname.replace('.shp','.shx'))
+
+for poly in shp:
+  c = poly.centroid
+  data = db.next()
+  table.append([p,c,data])
+  p = p+1
