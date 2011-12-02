1454e2867dc7c82f0b7cd7725a3f6dfe1678e812
stephanie.kleinschmidt@gmail.com <stephanie.kleinschmidt@gmail.com@a0262ff6-7a6d-11de-9755-51fe06bdd687>
2011-12-02 19:20:11 +0000

    This is the updated distance matrix file - it creates the distance matrix and a dictionary of sorted populations for each point

--- branches/gph4598f11/spacetime/distance_matrix.py
+++ branches/gph4598f11/spacetime/distance_matrix.py
@@ -9,8 +9,9 @@ Output should be a list that contains a list for each point pair
 """
 
 from math import sqrt
+import numpy as np
 
-#Input table : table = [point_num, weight, x-coord, y-coord]
+#Input table : table = [point_num, weight, x-coord, y-coord, total_pop]
 distances = []
 
 for point in table:
@@ -21,3 +22,29 @@ for point in table:
         y2 = other[3]
         dist = sqrt((x2-x1)**2 + (y2-y1)**2)
         distances.append([point[0], dist, other[0]])
+distances.sort()
+
+d = np.array(distances)
+nd = {}
+pop = []
+cpop = []
+for i in range(len(table)):
+    pop.append(table[i][4])
+    for k in distances[i]:
+        nd[i+1] = d[d[:,0] == i+1][:,2].tolist()
+for i in range(len(table)):
+    temp = []
+    for p in range(len(table)):
+        temp.append(pop[int(nd[i+1][p])-1])
+    cpop.append(temp)
+
+"""
+point_num = 1
+relationship_table = []
+for point in distances:
+    neighbors = []
+    if point[0] == point_num:
+        neighbors.append(point[2])
+    point_num += 1
+    relationship_table.append(neighbors)
+"""
